7ed0d5593c35822b8e02b109c4a4120357e0ebc9
dvpetrov <dvpetrov.main@gmail.com>
2017-07-17 18:14:20 +0300

    Register scenario

--- src/commands/acceptance.js
+++ src/commands/acceptance.js
@@ -4,10 +4,12 @@ const Command = require('@rispa/cli/src/Command')
 const { PROJECT_NAME } = require('../constants')
 const webpackBuild = require('../scenarios/webpackBuild')
 const storybookBuild = require('../scenarios/storybookBuild')
+const startServer = require('../scenarios/startServer')
 
 const scenarios = {
   'webpack-build': webpackBuild,
   'storybook-build': storybookBuild,
+  'start-server': startServer,
 }
 
 class AcceptanceCommand extends Command {
@@ -35,14 +37,17 @@ class AcceptanceCommand extends Command {
         title: 'Run scenarios',
         skip: () => !process.env.CI_ACCEPTANCE && 'Acceptance tests disabled',
         task: () => {
-          const scenarioList = scenarioNames.map(name => scenarios[name])
+          const scenarioList = scenarioNames.map(name => ({ name, task: scenarios[name] }))
 
-          const invalidScenarioList = scenarioList.filter(scenario => !scenario)
+          const invalidScenarioList = scenarioList
+            .filter(scenario => !scenario.task)
+            .map(scenario => scenario.name)
+
           if (invalidScenarioList.length > 0) {
             throw new Error(`Can't find scenarios: ${invalidScenarioList.join(', ')}`)
           }
 
-          return new Listr(scenarioList)
+          return new Listr(scenarioList.map(scenario => scenario.task))
         },
       },
     ])
